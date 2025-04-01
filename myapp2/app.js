22f87128678d296d7c89340fac70fe983d32d0d4
Nick Dimizas <nikkosdim@gmail.com>
2025-04-01 21:25:52 +0300

    feat: myapp4 added and usersApp created

--- myapp2/app.js
+++ myapp2/app.js
@@ -2,15 +2,44 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use('/', express.static('files'));
+
 app.get('/', (req, res) => {
-  res.send("This is the home pages");
+  res.send("This is the home page");
 });
 
 app.post('/user', (req, res) => {
   let data = req.body;
+  let username = req.body.username;
+  let email = req.body.email;
 
   console.log(data);
-  res.send("User page");
+  res.send(`User data ${username}, ${email}`);
+})
+
+app.post('/userForm', (req, res) => {
+  let data = req.body
+
+  console.log("Data", data);
+  res.send("UserForm Page");
+})
+
+app.use('/user1', (req, res) => {
+  console.log("User 1")
+  res.send("User 1 Page")
+})
+
+app.use('/user2', (req, res) => {
+  console.log("User 2")
+  res.send("User 2 Page")
+})
+
+app.use('/user2/hello', (req, res) => {
+  console.log("User 2 hello")
+  res.send("User 2 hello Page")
 })
 
 app.listen(port, () => {
---
+++ myapp3/app.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+
+app.get("/create", (req, res) => {
+  console.log("Create Page");
+  res.render('form', {});
+});
+
+app.post('/user', (req, res) => {
+  console.log("User Post");
+
+  let data = req.body;
+  console.log("Data", data);
+
+  let username = data.username;
+  let email = data.email;
+
+  res.render('user',
+    {
+      data1: username,
+      data2: email
+    }
+  );
+});
+
+app.get('/users', (req, res) => {
+  console.log("Users Page");
+
+  const users = [
+    {
+      "username": "nikos",
+      "email": "nikos@aueb.com"
+    },
+    {
+      "username": "maria",
+      "email": "maria@aueb.com"
+    }
+  ]
+
+  res.render("users", {users});
+});
+
+app.listen(port, () => {
+  console.log("Server is up");
+});
