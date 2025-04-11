11283dd90894044463e6f8174968d35027dd1451
Nick Dimizas <nikkosdim@gmail.com>
2025-04-11 04:05:38 +0300

    feat: files folder added and cors origined

--- usersApp/app.js
+++ usersApp/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const app = express()
+const app = express();
+const cors = require('cors');
 
 app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger');
@@ -10,15 +12,23 @@ const user = require('./routes/user.routes');
 const userProduct = require('./routes/user.products.routes')
 const auth = require('./routes/auth.routes');
 
+app.use(cors({
+  // origin: '*'
+  origin: ['http://localhost:3000']
+})
+)
+
 app.use('/api/auth', auth);
 app.use('/api/users', user);
 app.use('/api/user-product', userProduct);
+
+app.use('/', express.static('files'));
 
 app.use(
   '/api-docs',
   swaggerUi.serve,
   swaggerUi.setup(swaggerDocument.options)
- )
+ );
 
 module.exports = app
 
--- usersApp/controllers/user.controller.js
+++ usersApp/controllers/user.controller.js
@@ -12,10 +12,11 @@ exports.findAll = async(req, res) => {
     const result = await userService.findAll();
     res.status(200).json({status: true, data: result});
     logger.info("Success in reading all users");
-    logger.warn("Success in reading all users");
-    logger.error("Message with error");
+    // logger.warn("Success in reading all users");
+    // logger.error("Message with error");
   } catch (err) {
     console.log("Problem in reading users", err);
+    logger.error("Problem in reading all users", err);
     res.status(400).json({status:false, data: err});
   }
 }
